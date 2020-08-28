b31d63b5d4da062f29135b178209f85c9639e2f9
Beery Shklar <52495055+BeeryShklar@users.noreply.github.com>
2020-08-28 15:53:16 +0300

    The message event now ignores bots

--- bot/events/message.js
+++ bot/events/message.js
@@ -6,6 +6,8 @@ const prefix = '!'
  * @param {Discord.Message} msg
  */
 module.exports = msg => {
+	if (msg.author.bot) return new Error('Author is a bot')
+
 	const content = msg.content
 	const args = content.split(' ').splice(0, 1)
 	const command = content.split(' ')[0].startsWith(prefix)
@@ -15,6 +17,10 @@ module.exports = msg => {
 	if (command) matchCommands(msg, command, args, content)
 }
 
+/**
+ * @param {Discord.Message} msg
+ * @param {String} command
+ */
 function matchCommands(msg, command) {
 	switch (command) {
 		case 'hi':
