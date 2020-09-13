4558ce95f507d6d9c1bbef2b68476bfbbf47c24d
Beery Shklar <52495055+BeeryShklar@users.noreply.github.com>
2020-09-13 19:15:43 +0300

    Added more lines to error catch in `c` command

--- bot/events/commands/c.js
+++ bot/events/commands/c.js
@@ -20,13 +20,28 @@ const cb = async (args, _, msg) => {
 	const msgColor = await guildSettings.get('msg-color')
 	const warningColor = await guildSettings.get('warning-color')
 
-	const amountToDelete = parseInt(args[0])
-	const messagesToDelete = await msg.channel.messages.fetch({
-		limit: amountToDelete < 100 ? amountToDelete + 1 : 100,
-	})
-
 	try {
+		const amountToDelete = parseInt(args[0])
+		const messagesToDelete = await msg.channel.messages.fetch({
+			limit: amountToDelete < 100 ? amountToDelete + 1 : 100,
+		})
+
 		const messagesDeleted = await msg.channel.bulkDelete(messagesToDelete)
+
+		if (amountToDelete > 100) {
+			const warningMessage = await msg.channel.send(
+				new Discord.MessageEmbed()
+					.setColor(warningColor)
+					.setTitle(`Can't delete more than 100 messages at once`)
+			)
+			setTimeout(() => warningMessage.delete(), 2000)
+		}
+		const sentMessage = await msg.channel.send(
+			new Discord.MessageEmbed()
+				.setColor(msgColor)
+				.setTitle(`Deleted ${messagesDeleted.size - 1} messages`)
+		)
+		setTimeout(() => sentMessage.delete(), 2000)
 	} catch (err) {
 		msg.channel.send(
 			new Discord.MessageEmbed()
@@ -34,20 +49,6 @@ const cb = async (args, _, msg) => {
 				.setTitle(`An error occurred while trying to delete this messages :(`)
 		)
 	}
-	if (amountToDelete > 100) {
-		const warningMessage = await msg.channel.send(
-			new Discord.MessageEmbed()
-				.setColor(warningColor)
-				.setTitle(`Can't delete more than 100 messages at once`)
-		)
-		setTimeout(() => warningMessage.delete(), 2000)
-	}
-	const sentMessage = await msg.channel.send(
-		new Discord.MessageEmbed()
-			.setColor(msgColor)
-			.setTitle(`Deleted ${messagesDeleted.size - 1} messages`)
-	)
-	setTimeout(() => sentMessage.delete(), 2000)
 }
 
 const help = () => ({
